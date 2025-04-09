fb2ead86cec79eecff61e1359af73f173ae4580f
simon-shao-ics4u1-03-25 <simon.shao@student.tdsb.on.ca>
2025-04-09 17:31:58 +0000

    Complete Assignment

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,27 +1,101 @@
 public class PracticeProblem {
 
 	public static void main(String args[]) {
+	String[][] arr = new String[][] {
+		{"", "", "", ""},
+		{"", "", "", ""},
+		{"", "", "", ""},
+		{"S", "*", "F", "*"}
+	};
+	System.out.println(searchMazeMoves(arr));
+
+
 
 	}
 
-	public static void q1() {
-		//Write question 1 code here
-	}
 
-	public static void q2() {
-		//Write question 2 code here
-	}
+public static int searchMazeMoves(String[][] arr){
+	int row = arr.length-1;
+	int col = 0;
+	int move = 0;
 
-	public static void q3() {
-		//Write question 3 code here
-	}
+	move = mazeHelper(arr, row, col, move);
 
-	public static void q4() {
-		//Write question 4 code here
-	}
+	return move;
+}
 
-	public static void q5() {
-		//Write question 5 code here
-	}
+public static int mazeHelper(String[][] arr, int row, int col, int move){
+//base case
+if (row == -1){
+	return -1;
+}
+if (col == -1){
+	return -1;
+}
+if (row == arr.length){
+	return -1;
+}
+if (col == arr[row].length){
+	return -1;
+}
 
+
+if (arr[row][col].equals("*")){
+	return -1;
+}
+if (arr[row][col].equals("F")){
+	return move;
+}
+
+String tempSpace = arr[row][col];
+arr[row][col] = "*";
+
+move++;
+
+int temp1 = mazeHelper(arr, row, col+1, move);
+int temp2 = mazeHelper(arr, row-1, col, move);
+int temp3 = mazeHelper(arr, row, col-1, move);
+int temp4 = mazeHelper(arr, row+1, col, move);
+
+System.out.println(temp1 + "      1");
+System.out.println(temp2 + "      2");
+System.out.println(temp3 + "      3");
+System.out.println(temp4 + "      4");
+
+
+if(temp1!=-1){
+	if(temp1>temp2&&temp2!=-1){
+		temp1=temp2;
+		}
+}
+else{
+	temp1 = temp2;
+}
+
+if(temp3!=-1){
+	if(temp3>temp4&&temp4!=-1){
+		temp3=temp4;
+		}
+}
+else{
+	temp3 = temp4;
+}
+System.out.println(temp1 + "      1");
+System.out.println(temp2 + "      2");
+System.out.println(temp3 + "      3");
+System.out.println(temp4 + "      4");
+System.out.println("it ran");
+
+if(temp3!=-1){
+	if(temp3>temp1&&temp1!=-1){
+		temp3=temp1;
+		}
+}
+else{
+	temp3 = temp1;
+}
+move = temp3;
+arr[row][col] = tempSpace;
+return move;
+}
 }
